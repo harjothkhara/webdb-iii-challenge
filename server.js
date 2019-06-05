6acfa014f3553f3756061fda537d73e752c762a5
harjothkhara <harjoth.khara@gmail.com>
2019-06-05 14:58:54 -0700

    table creates using knex

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require("express");
+const helmet = require('helmet');
 
 const server = express();
+
+//middleware
+server.use(helmet());
+server.use(express.json());
 
 module.exports = server;
