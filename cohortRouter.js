1d0e7f0832c34e0dd0a451027c7232a690f340b8
harjothkhara <harjoth.khara@gmail.com>
2019-06-05 15:43:16 -0700

    new migration added for students table. Knex config added to cohortRouter

--- cohortRouter.js
+++ cohortRouter.js
@@ -1,4 +1,28 @@
 const express = require('express');
 const knex = require('knex');
 
+const knexConfig = {
+    client: "sqlite3",
+    connection: {
+        filename: './data/lambda.db3'
+    },
+    useNullAsDefault: true //needed for sqlite
+};
+
+const db = knex(knexConfig);
+
+const router = express.Router();
+
+router.get("/", (req, res) => {});
+
+router.get("/", (req, res) => {});
+
+router.post("/", (req, res) => {});
+
+router.delete("/", (req, res) => {});
+
+router.put("/", (req, res) => {});
+
+
+
 module.exports = router;
